e4b45f047aa0029b3159e17b6aa25cebf0425c92
Mattes Sarcander <info@fortschritt.pro>
2020-04-22 15:03:45 +0200

    Django 1.11 fixes

--- spaces_notifications/apps.py
+++ spaces_notifications/apps.py
@@ -1,13 +1,17 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+from .utils import create_notice_types
 
 class SpacesNotificationsConfig(AppConfig):
     name = 'spaces_notifications'
 
     def ready(self):
-        from .utils import register_notification
-        from django.utils.translation import ugettext_noop as _
-        register_notification(
-            'default',
-            _('Change Notification'),
-            _('Something has changed in one of your groups.')
-        )
+        #from .utils import register_notification
+        #from django.utils.translation import ugettext_noop as _
+        #register_notification(
+        #    'default',
+        #    _('Change Notification'),
+        #    _('Something has changed in one of your groups.')
+        #)
+        post_migrate.connect(create_notice_types, sender=self)
--- spaces_notifications/utils.py
+++ spaces_notifications/utils.py
@@ -1,13 +1,23 @@
-from collab.util import db_table_exists
-from pinax.notifications.models import NoticeType
+from django.conf import settings
+from django.utils.translation import ugettext_noop as _
 
 def register_notification(label, display, description):
-    """
-    """ 
-    if db_table_exists('notifications_noticetype'):
+    #if db_table_exists('notifications_noticetype'):
+    if "pinax.notifications" in settings.INSTALLED_APPS:
+        from pinax.notifications.models import NoticeType
         NoticeType.objects.get_or_create(
             label=label, 
             display=display, 
             description=description,
             default=2 # sadly hardcoded, see the named parameter 'default' in NoticeType.create()
         )
+
+
+def create_notice_types(sender, **kwargs): 
+    if "pinax.notifications" in settings.INSTALLED_APPS:
+        register_notification(
+            'default',
+            _('Change Notification'),
+            _('Something has changed in one of your groups.')
+        )
+
